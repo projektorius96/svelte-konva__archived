7c223757c9696d6b595d0c47b9237b6b64e95450
Lukas Gaucas <34185919+projektorius96@users.noreply.github.com>
2022-10-22 23:25:26 +0100

    register proposal, see desc.
    DEV_NOTE: I might need to rename the components to improve readability, refer to NEXT_GOAL: merge this to __component, than branch-off to a dedicated branch do the renaming part;

--- src/Konvelte--stage.svelte
+++ src/Konvelte--stage.svelte
@@ -3,38 +3,32 @@
     import {onMount} from 'svelte';
     import Konva from 'konva';
     import {default as KonvelteCircle} from './components/Konvelte--circle.svelte'
+    import {KonvelteComponentRegister as Register} from './utils/KonvelteComponentRegister'
     
     let isMounted = false;
     let k = 10;
     let [stage, layer] = [undefined, undefined];
-    let [KonvelteComponentRegister, KonvelteComponentRegisterInstance] = [undefined, Array(k).fill(0)];
-
-    $: if(isMounted) {
-
-        console.log("--Konvelte--stage dependent $$props.props: ", $$props.props);
-
-    }
-
-    $: if (stage && layer && KonvelteComponentRegister) {
-
-        console.log("stage && layer && KonvelteComponentRegister?", stage, layer, KonvelteComponentRegister)
-        
-    }
+    /* let [KonvelteComponentRegister, KonvelteComponentRegisterInstance] = [undefined, Array(k).fill(0)]; */// # DEV_NOTE : I AM COMMENTING THIS OUT, WILL GET BACK TO THIS SOON [1/2] :
+    let {_this} = Register(KonvelteCircle)
+    
+    // $: if (stage && layer/*  && KonvelteComponentRegister */) {
+    //     console.log("stage && layer && KonvelteComponentRegister?", stage, layer, KonvelteComponentRegister) 
+    // }
 
     $: if (isMounted) {
-    
-        KonvelteComponentRegister = new KonvelteCircle(/* {
-            target: $$props.props.sharedTarget,
-        }*//* NOTE : instead, just pass empty object to inherit target property from the parent, i.e.: */{}); // 1/4) register Konvelte component step  
-        KonvelteComponentRegisterInstance.forEach((val, idx, _arr)=>{
-            _arr[idx] = new KonvelteComponentRegister.KonvelteCircle({
-            /*  
-            
-            props: {
-                stage // pass the stage via dedicated props attribute as if <KonvelteCircle props={stage} />
-            }, 
-            
-            */
+
+        stage = new Konva.Stage({
+            container: /* 'container' */$$props.props.sharedTarget,
+            width: 960,
+            height: 480,
+        });
+        stage.setAttr('height', window.innerHeight); // # FIXES HEIGHT SCALING ISSUE
+        globalThis.KONVA_GLOBAL__STAGE = stage; // KONVA GLOBAL (DEBUGGING PURPOSES ONLY)
+
+        layer = new Konva.Layer();
+        stage.add(layer);
+
+        layer.add(_this.KonvelteCircle({
                 x: stage.width() / 2,
                 y: window.innerHeight / 2,
                 radius: 120,
@@ -42,36 +36,47 @@
                 stroke: 'black',
                 strokeWidth: 4,
                 draggable: true,
-            }) // 2/4) enqueue the Konvelte component
-            layer.add(_arr[idx])
-        })
-        
-        // ANNOT : THIS MAY BE REMOVED WITHIN A COUPLE OF UPCOMING COMMITS 
-        // // CRUCIAL_LINE : PLEASE DO NOT FORGET TO INVOKE requestAnimationFrame : 
-        // requestAnimationFrame(()=>{
-        //     /* layer.add(KonvelteComponentRegisterInstance) */// 3/4)
-        //     /* layer.draw() */ 4/4)
-        // })
+        }))
+    
+        // DEV_NOTE : I AM COMMENTING THIS OUT, WILL GET BACK TO THIS SOON [2/2] :
+        // KonvelteComponentRegister
+        // = new KonvelteCircle(
+        // /* {
+        //     target: $$props.props.sharedTarget,
+        // }
+        // *//* NOTE : instead, just pass empty object to inherit target property from the parent, i.e.: */
+        // {}
+        // ); // 1/4) register Konvelte component step  
+        // KonvelteComponentRegisterInstance.forEach((val, idx, _arr)=>{
+            
+        //     _arr[idx] = new KonvelteComponentRegister.KonvelteCircle({
+        //     /*  
+            
+        //     props: {
+        //         stage // pass the stage via dedicated props attribute as if <KonvelteCircle props={stage} />
+        //     }, 
+            
+        //     */
+        //         x: stage.width() / 2,
+        //         y: window.innerHeight / 2,
+        //         radius: 120,
+        //         fill: 'red',
+        //         stroke: 'black',
+        //         strokeWidth: 4,
+        //         draggable: true,
+        //     }) // 2/4) enqueue the Konvelte component
+        //     layer.add(_arr[idx])
 
-        stage.setAttr('height', window.innerHeight); // # FIXES HEIGHT SCALING ISSUE
+        // })
     
     }
 
     onMount(()=>{
 
+        /* console.log("What are --Konvelte--stage dependent $$props.props?", $$props.props); */
         console.log("onMount ([pathspec].svelte)");
         isMounted = true;
 
     })
-
-    stage = new Konva.Stage({
-        container: /* 'container' */$$props.props.sharedTarget,
-        width: 960,
-        height: 480,
-    });
-    globalThis.KONVA_GLOBAL__STAGE = stage; // KONVA GLOBAL (DEBUGGING PURPOSES ONLY)
-
-    layer = new Konva.Layer();
-    stage.add(layer);
 
 </script>
