596d9626c28382f8c24679f5623a5ecbd7bf29c4
Lukas Gaucas <34185919+projektorius96@users.noreply.github.com>
2022-10-23 21:09:34 +0100

    cleaner code, see desc.
    DEV_NOTE: we might need to polish this component on the current branch before PR, but we moving towards minimum functional boilerplate for svelte-konva for sure;

--- src/kStage.svelte
+++ src/kStage.svelte
@@ -4,7 +4,7 @@
     import {Stage} from 'konva/lib/Stage';
     import {default as Circle} from './components/kCircle.svelte';
     import {default as Layer} from './components/kLayer.svelte';
-    import {/* KonvelteComponentRegister as Register,  */KonvelteComponentEnqueuer as Enqueuer} from './utils/KonvelteComponentRegister';
+    import {KonvelteComponentEnqueuer as Enqueuer} from './utils/KonvelteComponentRegister';
     
     // lexical props
     export let width = null;
@@ -13,35 +13,16 @@
     // lexical variables
     let isMounted = false;
     let stage = undefined;
-    let k = 10;
+    let k = 10; // KTH-tuple of Konva.Shape(s)
 
     // lexical Registers
     let LayerInstance1 = Enqueuer(Layer);
-    // let CircleInstance0 = /* Register(Circle) */Enqueuer(Circle, {
-    //             x: stage.width() / 2,
-    //             y: window.innerHeight / 2,
-    //             radius: 120,
-    //             fill: 'red',
-    //             stroke: 'black',
-    //             strokeWidth: 4,
-    //             draggable: true,
-    // })
+    $: if(stage && LayerInstance1) {
 
-    // $: if (LayerInstance1) console.log(LayerInstance1);
-    // $: if (stage && Enqueuer) {
-    //     console.log("Enqueuer (useEffect)", Enqueuer(Circle, {
-    //             x: stage.width() / 2,
-    //             y: window.innerHeight / 2,
-    //             radius: 120,
-    //             fill: 'red',
-    //             stroke: 'black',
-    //             strokeWidth: 4,
-    //             draggable: true,
-    //     }))
-    // }
+        stage.add(LayerInstance1);
 
-    $: if(stage) {
         Array(k).fill(0).forEach((_, idx, _arr)=>{
+            
             _arr[idx] = Enqueuer(Circle, {
                 x: stage.width() / 2,
                 y: window.innerHeight / 2,
@@ -54,7 +35,11 @@
             if (idx == _arr.length - 1) {
                 LayerInstance1.add(..._arr)
             }
+
         })
+
+        globalThis.LayerInstance1 = LayerInstance1; /* <= DEBUGGING PURPOSE ONLY *//* LayerInstance1.destroy() */// [PASSED]
+
     }
 
     $: if (isMounted) {
@@ -68,14 +53,12 @@
         
         /* globalThis.KONVA_GLOBAL__STAGE = stage; */ // KONVA GLOBAL (DEBUGGING PURPOSES ONLY)
 
-        // layer = LayerInstance1/* .Layer() */
-        stage.add(LayerInstance1);
     }
 
     onMount(()=>{
 
-        /* console.log("$$props.props at [pathspec].svelte", $$props.props); */
-        console.log("onMount ([pathspec].svelte)");
+        console.log("$$props.props at [pathspec].svelte", $$props.props);
+        console.log("onMount at [pathspec].svelte");
         isMounted = true;
 
     })
