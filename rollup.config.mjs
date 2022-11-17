62c8d22e832979b48682092d9e8aca02012cc6be
Lukas Gaucas <34185919+projektorius96@users.noreply.github.com>
2022-11-17 20:23:37 +0000

    missing deps to manual link it, see desc.
    - accidently noticed missing dependencies (deps) for manual linking [SOLVED], I assume I've done via CLI globally installed Rollup, that's why I have not collided with an issue up until now, sorry for that ;

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,9 +2,12 @@ import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
     
 export default {
-        input: 'src/_index.js',
+        input: 'src/index.js',
         output: [
-            { file: './dist/svelte-konva-linked/index.js', 'format': 'esm' },
+            { 
+                file: './dist/svelte-konva-linked/index.js',
+                format: 'esm' 
+            },
         ],
         plugins: [
             svelte(),
